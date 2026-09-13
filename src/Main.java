bc3c0f83da6beb91258062c083f95419695dabdf
beecheverriap-beep <be.echeverriap@duocuc.cl>
2026-09-13 00:43:02 -0300

    Primer comentario

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        // Edad utilizada para evaluar una regla del sistema
+        int edad = 20;
+        double descuento = 0.10;
+
+        double total = 50000;
+        /*
+        * El calculo aplica el descuento definido
+        * antes de obtener el total final
+         */
+        double totalFinal = total - (total * descuento);
+
+    }
+}
