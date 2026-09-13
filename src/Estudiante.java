b11e6f3fe7d3053e7670803f16cf43d3740b6c71
beecheverriap-beep <be.echeverriap@duocuc.cl>
2026-09-13 18:55:14 -0300

    Se

--- src/Estudiante.java
+++ src/Estudiante.java
@@ -6,12 +6,12 @@ public class Estudiante {
     private int edad;
 
     /**
-     * Determina si el estudiante aprueba segun su nota final
-     * @param notaFinal nota obtenidad por el estudiante
-     * @return true si la nota final es igual o superior a 4.0
+     * Determina si el estudiante aprueba según su nota final.
+     *
+     * @param notaFinal nota obtenida por el estudiante
+     * @return true si la nota es igual o superior a 4.0
      */
-
-    public boolean estaAprobado(double notaFinal){
-        return notaFinal>= 4.0;
+    public boolean estaAprobado(double notaFinal) {
+        return notaFinal >= 4.0;
     }
 }
